5bedc6befe0b58bd71f45afefe28164f31fad767
Petr Dědic <petandedic@gmail.com>
2024-11-18 18:10:23 +0100

    Update build errors

--- ttc-performance/lib/supabaseClient.js
+++ ttc-performance/lib/supabaseClient.js
@@ -2,11 +2,13 @@
 
 import { createClient } from "@supabase/supabase-js";
 
-// Initialize the Supabase client with environment variables
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-// Create a single Supabase client instance
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Supabase environment variables are not set!");
+}
+
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export default supabase;
--- ttc-performance/next.config.js
+++ ttc-performance/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    styledComponents: {
+      ssr: true, // Enable Server-Side Rendering for styled-components
+      displayName: true, // Add display names for easier debugging
+    },
+  },
   async redirects() {
     return [
       {
