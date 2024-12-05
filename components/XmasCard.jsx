1ada6c69b7b604ab432bf0f6f83f0be8f886bf1b
Petr Dědic <petandedic@gmail.com>
2024-12-05 06:00:33 +0100

    Xmas card

--- components/XmasCard.jsx
+++ components/XmasCard.jsx
@@ -15,19 +15,128 @@ export default function XmasCard() {
 
   return (
     <Card
-      style={{ boxShadow: "rgba(0, 0, 0, 0.25) 0px 5px 15px" }}
       w={smallWindow ? "80vw" : "60vw"}
       mx="auto"
       mb={72}
       radius={16}
-      px={32}
-      py={24}
+      bg="none"
+      p={0}
+      style={{ overflow: "visible" }}
     >
-      <Flex gap={64} direction={smallWindow ? "column" : "row"} align="center">
+      <Flex
+        gap={smallWindow ? 24 : 64}
+        direction={smallWindow ? "column" : "row"}
+        align="center"
+        style={{ overflow: "visible" }}
+      >
+        <Flex
+          gap={16}
+          w="100%"
+          h="100%"
+          direction={smallWindow ? "column" : "row"}
+          style={{ overflow: "visible" }}
+        >
+          <Stack
+            w="100%"
+            px={16}
+            py={32}
+            gap={24}
+            align="center"
+            bg="white"
+            style={{
+              boxShadow: "rgba(0, 0, 0, 0.25) 0px 5px 15px",
+              borderRadius: 32,
+            }}
+          >
+            <Stack gap={0} align="center" w="100%">
+              <Text fz={24} fw={700} ta="center">
+                Dárkové poukazy
+              </Text>
+              <Text ta="center" fz={14} w="100%">
+                V hodnotě{" "}
+                <Text span style={{ textWrap: "nowrap" }} fz={14}>
+                  10 000
+                </Text>{" "}
+                &{" "}
+                <Text span style={{ textWrap: "nowrap" }} fz={14}>
+                  15 000 Kč.
+                </Text>
+              </Text>
+            </Stack>
+
+            <AspectRatio
+              ratio={1350 / 1000}
+              w="100%"
+              h="100%"
+              style={{
+                position: "relative",
+                borderRadius: 16,
+              }}
+            >
+              <Image
+                quality={100}
+                src="/Fotky_WEBP/Darkove_poukazy_v1.webp"
+                alt="/Fotky_WEBP/Darkove_poukazy_v1.webp"
+                fill
+                sizes="100vw"
+                style={{ borderRadius: 16, objectFit: "cover" }}
+              />
+            </AspectRatio>
+          </Stack>
+          <Stack
+            w="100%"
+            px={16}
+            py={32}
+            gap={24}
+            align="center"
+            bg="white"
+            style={{
+              boxShadow: "rgba(0, 0, 0, 0.25) 0px 5px 15px",
+              borderRadius: 32,
+            }}
+          >
+            <Stack gap={0} align="center" w="100%">
+              <Text fz={24} fw={700} ta="center">
+                Dárkové poukazy
+              </Text>
+              <Text ta="center" fz={14}>
+                V hodnotě{" "}
+                <Text span style={{ textWrap: "nowrap" }} fz={14}>
+                  3 000,
+                </Text>{" "}
+                <Text span style={{ textWrap: "nowrap" }} fz={14}>
+                  5 000
+                </Text>{" "}
+                &{" "}
+                <Text span style={{ textWrap: "nowrap" }} fz={14}>
+                  7 000 Kč.
+                </Text>
+              </Text>
+            </Stack>
+
+            <AspectRatio
+              ratio={1350 / 1000}
+              w="100%"
+              h="100%"
+              style={{
+                position: "relative",
+                borderRadius: 16,
+              }}
+            >
+              <Image
+                quality={100}
+                src="/Fotky_WEBP/Darkove_poukazy_v2.webp"
+                alt="/Fotky_WEBP/Darkove_poukazy_v2.webp"
+                fill
+                sizes="100vw"
+                style={{ borderRadius: 16, objectFit: "cover" }}
+              />
+            </AspectRatio>
+          </Stack>
+        </Flex>
         <Stack w="100%" justify="space-between" h="100%">
-          <Text fz={20} fw={700} c="#e84048">
-            Darujte svým nejbližším pod stromeček dárkový poukaz od TTC
-            Performance!
+          <Text fz={32} fw={700} lh={1.25}>
+            Darujte svým nejbližším dárkový poukaz!
           </Text>
           <List withPadding>
             <List.Item>Platí pouze pro softwarovou úpravu</List.Item>
@@ -39,39 +148,31 @@ export default function XmasCard() {
               Hodnota poukazů je 3 000, 5 000, 7 000, 10 000, a 15 000 Kč
             </List.Item>
           </List>
-          <Text>
-            Pro více informací nás kontaktujte na{" "}
-            <Link
-              href="tel:+420 602 562 650"
+          <Text fz={24}>
+            Pro více informací nás kontaktujte{" "}
+            <Text
+              span
+              fz={24}
               style={{
-                fontWeight: 700,
+                textWrap: "nowrap",
                 color: "black",
               }}
             >
-              +420 602 562 650
-            </Link>
-            .
+              na{" "}
+              <Link
+                href="tel:+420 602 562 650"
+                style={{
+                  fontWeight: 700,
+                  color: "black",
+                  textWrap: "nowrap",
+                }}
+              >
+                +420 602 562 650
+              </Link>
+              .
+            </Text>
           </Text>
         </Stack>
-
-        <AspectRatio
-          ratio={1400 / 1000}
-          w="100%"
-          h="100%"
-          style={{
-            position: "relative",
-            borderRadius: 16,
-          }}
-        >
-          <Image
-            quality={100}
-            src="/Fotky_WEBP/Darkovy_poukaz_web.webp"
-            alt="/Fotky_WEBP/Darkovy_poukaz_web.webp"
-            fill
-            sizes="100vw"
-            style={{ borderRadius: 16, objectFit: "cover" }}
-          />
-        </AspectRatio>
       </Flex>
     </Card>
   );
